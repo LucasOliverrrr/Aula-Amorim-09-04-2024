0c329fe48f161eda7673a3da7ac1deddb76d8381
LucasOliverrrr <lucasouze@gmail.com>
2024-04-09 10:44:03 -0300

    Lista feita

---
+++ main.js
@@ -0,0 +1,9 @@
+import todolist from "./todolist.js"
+
+//console.log("oi todolist", todolist)
+
+todolist.add("Ir ao cinema", false)
+todolist.add("Pagar conta", false)
+todolist.add("Estudar JavaScript", false)
+
+console.log("oi todolist", todolist)
